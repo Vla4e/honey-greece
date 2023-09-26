ade6ca00e682ae8d535aff84bfc5b700d3a49ddb
Vladimir Nikolovski <vlade_nikolovski@hotmail.com>
2023-09-26 15:03:06 +0200

    Fonts added correctly

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import '@/assets/styles/fonts.scss'
 import App from './App.vue'
 import router from './router'
 import components from './components'
